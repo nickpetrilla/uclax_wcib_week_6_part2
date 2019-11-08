d01a37c3fff97d17e8e8a531fcfe608fcb5009a0
nick imac <nickpetrilla@gmail.com>
2019-11-07 21:06:30 -0800

    Html update with video background & css. JS needs to work still.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log('Hello from js/scripts.js!');
+
+
+document.getElementById("get-some")
+        .addEventListener("click", function() {
+  document.getElementById("emtpy-start").hidden = true;
+  document.getElementById("reveal-some").hidden = false;
+}, false);
