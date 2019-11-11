3cee026ef06a5ec37a5609c2b813da9ca8f6a700
nick imac <nickpetrilla@gmail.com>
2019-11-11 11:32:27 -0800

    Updated JS + Modal

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,7 @@ let description= document.querySelector('#reveal-some');
 
 
 document.getElementById('get-some').onclick = function revealDescription(){
-description.textContent = 'IceSnake is the premium energy drink guaranteed to get you slithering! Gulp down this artic beverage and prepare to RIDE THE SNAKE! Made with 100% bold flavors and a few secret ingredients, IceSnake is the only drink to combine the power of the reptile and the temperature of frozen water.';
+
+        description.textContent = 'IceSnake is the premium energy drink guaranteed to get you slithering! Gulp down this artic beverage and prepare to RIDE THE SNAKE! Made with 100% bold flavors and a few secret ingredients, IceSnake is the only drink to combine the power of the reptile and the temperature of frozen water.';
+
 }
